a8baf42a0cf7a86d7d78fc826e3449690bc1946d
Liam Barkley <24899518@sun.ac.za>
2022-06-29 13:47:43 +0200

    made even more modular for list instead

--- Initial Population Scripts/populate_user_rights.sql
+++ Initial Population Scripts/populate_user_rights.sql
@@ -4,7 +4,7 @@ INSERT INTO dbo.User_File_Types
      (User_ID, Type_ID)
      SELECT dbo.Users.User_ID, dbo.File_Types.Type_ID 
 	 FROM dbo.Users, dbo.File_Types
-     WHERE dbo.Users.User_Name = 'matthewkeys' OR dbo.Users.User_Name = 'liambarkley';
+     WHERE dbo.Users.User_Name IN ('matthewkeys','liambarkley')
 
 SELECT TOP (10) [User_ID] ,[Type_ID]
   FROM [dbo].[User_File_Types]
