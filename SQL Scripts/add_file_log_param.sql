d1ee5ec166d60b2fefc09e6510e0264ef02cef22
Matthew Keys <25337971@sun.ac.za>
2022-06-30 12:20:28 +0200

    Added script to stage data from temp table

--- SQL Scripts/add_file_log_param.sql
+++ SQL Scripts/add_file_log_param.sql
@@ -2,9 +2,9 @@ USE [project-x]
 GO
 
 EXEC dbo.AddFileLog 
-	@File_ID = 0,
-	@Status = 'Status',
-	@Description = 'Description'
+	@File_ID = 6,
+	@Status = 'Successful',
+	@Description = NULL;
 
 GO
 
