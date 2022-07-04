19bf04bedde7b0efaa04ce229dcd86eabfad2990
Liam Barkley <24899518@sun.ac.za>
2022-07-04 15:38:44 +0200

    script working

--- SQL Scripts/stg_to_Fact.sql
+++ SQL Scripts/stg_to_Fact.sql
@@ -1,13 +1,4 @@
--- get date of last file added
-DECLARE @lastDate datetime = 
-(SELECT MAX(Date) AS 'Max' FROM (SELECT Files.File_ID AS ID, Files.Insert_Date AS Date
-FROM stg_HIV
-INNER JOIN Files ON stg_HIV.File_ID = Files.File_ID GROUP BY Files.File_ID, Insert_Date) s)
-
--- get File_ID of last file added
-DECLARE @lastFileID int = (SELECT File_ID FROM Files WHERE Insert_Date = @lastDate)
-/****** Need to change so that it gets the IDs of the latest file for each week ******/
-TRUNCATE TABLE FACTS
+/* Loading dimension tables   */
 
 -- Load diseases not yet in dimDisease
 INSERT INTO [dbo].[dimDisease]
@@ -17,30 +8,44 @@ LEFT JOIN dimDisease dd
 ON ft.Disease_Name = dd.Disease_Name
 WHERE dd.Disease_ID IS NULL
 
---insert from hiv staging table
-	INSERT INTO [dbo].[Facts]
+-- Load facilities not yet in dimFacility
+INSERT INTO [dbo].[dimFacilities]
+		([Facility_Province])
+SELECT DISTINCT
+	f.Facility_Province
+FROM Facilities f
+	LEFT JOIN dimFacilities d
+		ON d.Facility_Province = f.Facility_Province
+	WHERE d.Facility_ID IS NULL
+
+/* ETL: Write data from OLTP into OLAP  */
+
+-- Create temp table to store info from all staging tables
+SELECT * 
+INTO #temp 
+FROM stg_HIV
+UNION
+SELECT * FROM stg_TB
+
+-- Write to Facts
+INSERT INTO [dbo].[Facts]
            ([Data_Element],
            [Data_Element_Value],
 		   [DateKey], [Disease_ID], [Facility_ID])
+
 	SELECT stgTemp.Data_Element, stgTemp.Data_Element_Value, stgTemp.DateKey, stgTemp.Disease_ID, stgTemp.Facility_ID
-	FROM (SELECT hiv.Data_Element, hiv.Data_Element_Value, dd.DateKey, ds.Disease_ID, df.Facility_ID, Insert_Date
-		FROM stg_HIV hiv 
+	FROM 
+	-- Subquery to generate necessary columns
+	(SELECT t.Data_Element, t.Data_Element_Value, dd.DateKey, ds.Disease_ID, df.Facility_ID, Insert_Date
+		FROM #temp t 
 		INNER JOIN Files f 
-			ON hiv.File_ID = f.File_ID 
+			ON t.File_ID = f.File_ID 
 		INNER JOIN Weeks w 
 			ON f.Week_ID = w.Week_ID  
 		INNER JOIN DimDate dd
 			ON dd.WeekOfYear = w.Week
 			AND dd.Year = w.Year
 			AND dd.DateKey = (SELECT Max(DateKey) FROM DimDate dSub WHERE dSub.WeekOfYear = w.Week AND dSub.Year = w.Year) -- return max day of week
-		INNER JOIN (
-			SELECT Week_ID, MAX(f.Insert_Date) maxdate
-			FROM stg_HIV hiv
-				INNER JOIN Files f
-					ON hiv.File_ID = f.File_ID
-			GROUP BY Week_ID
-		) maxdatetbl
-		ON f.Insert_Date = maxdatetbl.maxdate
 		INNER JOIN File_Types ft
 			ON ft.Type_ID = f.Type_ID
 		INNER JOIN dimDisease ds
@@ -49,58 +54,24 @@ WHERE dd.Disease_ID IS NULL
 			ON f.Facility_ID = fa.Facility_ID
 		INNER JOIN dimFacilities df
 			ON fa.Facility_Province = df.Facility_Province
-		) stgTemp
+			-- Get latest file of week for every type and facility
+		INNER JOIN (
+			SELECT Week_ID, MAX(f.Insert_Date) maxdate, Type_ID, Facility_ID
+			FROM #temp t
+				INNER JOIN Files f
+					ON t.File_ID = f.File_ID
+			GROUP BY Week_ID, Type_ID, Facility_ID
+		) md
+		ON md.maxdate = f.Insert_Date
+		AND md.Facility_ID = f.Facility_ID
+		AND md.Type_ID = f.Type_ID) stgTemp
+		-- Join only files for week, type and facility not yet included
 	LEFT JOIN Facts 
 	ON stgTemp.DateKey = Facts.DateKey 
+	AND stgTemp.Facility_ID = Facts.Facility_ID
+	AND stgTemp.Disease_ID = Facts.Disease_ID
 	WHERE Facts.DateKey IS NULL
+	AND Facts.Facility_ID IS NULL
+	AND Facts.Disease_ID IS NULL
 
-
-	WHERE
-		Insert_Date = (
-		SELECT MAX(fLast.Insert_Date)
-		FROM Files fLast
-		INNER JOIN Weeks ON w.Week_ID = fLast.Week_ID
-		)
-
-SELECT hiv.Data_Element, hiv.Data_Element_Value, DateKey, f.Insert_Date
-	FROM stg_HIV hiv
-		INNER JOIN Files f 
-			ON hiv.File_ID = f.File_ID 
-		INNER JOIN Weeks w 
-			ON f.Week_ID = w.Week_ID  
-		INNER JOIN DimDate dd
-			ON dd.WeekOfYear = w.Week
-			AND dd.Year = w.Year
-			AND dd.DateKey = (SELECT Max(DateKey) FROM DimDate dSub WHERE dSub.WeekOfYear = w.Week AND dSub.Year = w.Year) -- return max day of week
-
-		
-
-SELECT Week, Max(Insert_Date)
-FROM Files f 
-	INNER JOIN Weeks w
-	ON w.Week_ID = f.Week_ID
-GROUP BY Week
-		
-/****** Need to change so that script can work for a general staging table ******/
-
--- get date of last file added
-SET @lastDate = 
-(SELECT MAX(Date) AS 'Max' FROM (SELECT Files.File_ID AS ID, Files.Insert_Date AS Date
-FROM stg_TB
-INNER JOIN Files ON stg_TB.File_ID = Files.File_ID GROUP BY Files.File_ID, Insert_Date) s)
-
--- get File_ID of last file added
-SET @lastFileID = (SELECT File_ID FROM Files WHERE Insert_Date = @lastDate)
-
---insert from tb staging table
-	INSERT INTO [dbo].[Facts]
-           ([Data_Element]
-           ,[Data_Element_Value],
-		   [DateKey])
-	SELECT Data_Element, Data_Element_Value, DateKey
-	FROM DimDate, stg_TB
-		INNER JOIN Files ON stg_TB.File_ID = Files.File_ID 
-		INNER JOIN Weeks ON Files.Week_ID = Weeks.Week_ID WHERE Files.File_ID = @lastFileID 
-			-- get DateKey from dimDate
-			AND WeekOfYear = (SELECT Week From Weeks WHERE 
-					Week_ID = (SELECT Week_ID FROM Files WHERE File_ID = stg_TB.File_ID))
+	DROP TABLE #temp
